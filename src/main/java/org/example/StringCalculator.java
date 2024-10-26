4752fbac45579828f112c24596b2d87e5a146de5
FaiselKhan <faisel.khan@humanclound.co.in>
2024-10-26 11:57:23 +0530

    first commit - test case failed for empty string

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -3,6 +3,6 @@ package org.example;
 public class StringCalculator {
 
     public int add(String numbers){
-
+        throw new IllegalArgumentException();
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,4 +1,26 @@
+import org.example.StringCalculator;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class StringCalculatorTest {
 
 
+    StringCalculator stringCalculator;
+
+    @BeforeEach
+    public void getInstance(){
+        stringCalculator = new StringCalculator();
+    }
+
+    @AfterEach
+    public void destroy(){
+        stringCalculator = null;
+    }
+
+    @Test
+    public void get_zero_for_empty_string_number(){
+        Assertions.assertEquals(0, stringCalculator.add(""));
+    }
 }
