8cf8c11adac3984e2eea63ccc35c8ccfcb89558c
FaiselKhan <faisel.khan@humanclound.co.in>
2024-10-26 12:10:57 +0530

    sixth commit - test case passed for two numbers

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -3,10 +3,15 @@ package org.example;
 public class StringCalculator {
 
     public int add(String numbers){
-        if(numbers.length() == 0){
-            return 0;
-        }else{
-            return Integer.parseInt(numbers);
+        if(numbers.length() < 2){
+            if(numbers.length() == 0){
+                return 0;
+            }else{
+                return Integer.parseInt(numbers);
+            }
         }
+
+        return Integer.parseInt(numbers.split(",")[0].trim())+Integer.parseInt(numbers.split(",")[1].trim());
     }
+
 }
